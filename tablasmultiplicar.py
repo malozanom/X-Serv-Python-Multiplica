50e648efe87806a3d1385b3f66462d205b19953b
Your Name <you@example.com>
2017-01-24 18:54:07 +0100

    programa hecho

---
+++ tablasmultiplicar.py
@@ -0,0 +1,8 @@
+# Programa que imprime las tablas de multiplicar.
+
+for fila in range (1,11):
+	print ("Tabla del " + str (fila))
+	print ("-----------")
+	for columna in range (1,11):
+		print (str(fila) + " por " + str(columna) + " es " + str(fila*columna))
+
